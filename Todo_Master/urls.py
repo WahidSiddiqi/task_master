9cca011c7ff1bc17b153f043f14e3c5befa5de8d
Kateattardo <youremail@domain.com>
2023-09-17 13:08:32 -0400

    fix task views

--- Todo_Master/urls.py
+++ Todo_Master/urls.py
@@ -20,5 +20,4 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- main_app/urls.py
+++ main_app/urls.py
@@ -14,14 +14,8 @@ urlpatterns = [
          views.ProjectUpdate.as_view(), name='projects_update'),
     path('projects/<int:pk>/delete/',
          views.ProjectDelete.as_view(), name='projects_delete'),
-    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
-    path('projects/<int:pk>/update/',
-         views.ProjectUpdate.as_view(), name='projects_update'),
-    path('projects/<int:pk>/delete/',
-         views.ProjectDelete.as_view(), name='projects_delete'),
     # TASKS
     path('tasks/', views.task_list, name='task_list'),
-    path('tasks/', views.task_list, name='task_list')
     # COMMENTS
     # REGISTRATION
     path('accounts/signup/', views.signup, name='signup'),
